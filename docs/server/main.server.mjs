01e71a517ce187c45757c743368076e68dbb4707
Laurentiu Cretu <contact.alccg@gmail.com>
2025-05-19 12:33:34 +0300

    redeploy

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-HCWJ5WIG.mjs";import"./chunk-4ZHOZXEN.mjs";import"./chunk-JHFZXWNI.mjs";import"./chunk-5TIFTACQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
